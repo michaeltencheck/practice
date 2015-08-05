029abcdf4d5c43907a38dfec0372a362c8bd56d6
michael <neo21anderson@gmail.com>
2015-08-05 22:48:59 +0800

    JSONTest
    JSONTest app

---
+++ JSONTest/app/src/main/java/com/example/test/jsontest/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.test.jsontest;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button button;
+    private TextView textView;
+    private static final int SHOW_RESPONSE = 0;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case SHOW_RESPONSE:
+                    String response = (String) msg.obj;
+                    textView.setText(response);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.send_request_button);
+        textView = (TextView) findViewById(R.id.show_textView);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpURLConnection httpURLConnection = null;
+                    URL url = new URL("http://192.168.4.11/get_data.json");
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setReadTimeout(8888);
+                    httpURLConnection.setReadTimeout(8888);
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader
+                            (new InputStreamReader(inputStream));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = "";
+                    while((line = bufferedReader.readLine()) != null){
+                        stringBuilder.append(line);
+                    }
+                    String repsonse = stringBuilder.toString();
+                    parseJSONWithJSONObject(repsonse);
+                    /*Message message = new Message();
+                    message.what = SHOW_RESPONSE;
+                    message.obj = stringBuilder.toString();
+                    handler.sendMessage(message);*/
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+
+    private void parseJSONWithJSONObject(String repsonse) {
+        try {
+            JSONArray jsonArray = new JSONArray(repsonse);
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String version = jsonObject.getString("version");
+                String name = jsonObject.getString("name");
+                Log.d("id", id);
+                Log.d("version", version);
+                Log.d("name", name);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}

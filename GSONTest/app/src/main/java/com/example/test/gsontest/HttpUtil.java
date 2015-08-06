abe28ae57fe4a485f9d7b0ac35650d5c1a5e604b
michael <neo21anderson@gmail.com>
2015-08-06 13:11:32 +0800

    open a subthread
    open a subthread

--- GSONTest/app/src/main/java/com/example/test/gsontest/HttpUtil.java
+++ GSONTest/app/src/main/java/com/example/test/gsontest/HttpUtil.java
@@ -11,31 +11,42 @@ import java.net.URL;
  * Created by test on 8/6/2015.
  */
 public class HttpUtil {
-    public static String sendHttpRequest(String address){
-        HttpURLConnection httpURLConnection = null;
-        String response = "";
-        try {
-            URL url = new URL(address);
-            httpURLConnection = (HttpURLConnection)url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-            httpURLConnection.setReadTimeout(8888);
-            httpURLConnection.setConnectTimeout(8888);
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader
-                    (new InputStreamReader(inputStream));
-            StringBuilder stringBuilder = new StringBuilder();
-            String line = "";
-            while ((line = bufferedReader.readLine()) != null){
-                stringBuilder.append(line);
+    public static void sendHttpRequest(final String address,
+                                       final HttpCallbackListener httpCallbackListener) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection httpURLConnection = null;
+                String response = "";
+                try {
+                    URL url = new URL(address);
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setReadTimeout(8888);
+                    httpURLConnection.setConnectTimeout(8888);
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader
+                            (new InputStreamReader(inputStream));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = "";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    response = stringBuilder.toString();
+                    if (httpCallbackListener != null){
+                        httpCallbackListener.onFinish(response);
+                    }
+                } catch (Exception e) {
+                    if (httpCallbackListener != null){
+                        httpCallbackListener.onError(e);
+                    }
+                } finally {
+                    if (httpURLConnection != null) {
+                        httpURLConnection.disconnect();
+                    }
+                }
             }
-            response = stringBuilder.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-            if(httpURLConnection != null){
-                httpURLConnection.disconnect();
-            }
-        }
-        return response;
+        }).start();
     }
 }

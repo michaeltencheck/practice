fcfc77d5d148191f5488d79cde5b18d81c99254b
michael <neo21anderson@gmail.com>
2015-08-06 16:21:27 +0800

    create locationmanager
    create locationmanager

--- LocationTest/app/src/main/java/com/example/test/locationtest/MainActivity.java
+++ LocationTest/app/src/main/java/com/example/test/locationtest/MainActivity.java
@@ -1,18 +1,40 @@
 package com.example.test.locationtest;
 
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private String provider;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.show_location_textView);
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        List<String> location_service = locationManager.getProviders(true);
+        if (location_service.contains(LocationManager.GPS_PROVIDER)){
+            provider = LocationManager.GPS_PROVIDER;
+        }else if (location_service.contains(LocationManager.NETWORK_PROVIDER)){
+            provider = LocationManager.NETWORK_PROVIDER;
+        }else {
+            Toast.makeText(this, "nothing at all", Toast.LENGTH_LONG ).show();
+            return;
+        }
     }
 
     @Override

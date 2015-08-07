9a17f379a555a2df9c841c6819f52589ba5fa39e
michael <neo21anderson@gmail.com>
2015-08-07 11:29:27 +0800

    add a ondestroy()
    add a ondestroy()

--- LocationTest/app/src/main/java/com/example/test/locationtest/MainActivity.java
+++ LocationTest/app/src/main/java/com/example/test/locationtest/MainActivity.java
@@ -73,6 +73,14 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null){
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
